a34816a6d50bff95f0772eb9de10e60a565d891d
LuisGerar321 <Luis.Camara@theksquaregroup.com>
2023-04-21 11:57:08 -0500

    fix endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from "express";
 import userRoutes from "./Routes/User";
+import bankAccounts from "./Routes/BankAccounts";
 
 export const app = express();
 const port = 5000;
 
 app.use(express.json());
 app.use("/users", userRoutes)
+app.use("/bankaccounts", bankAccounts)
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send({message: "Hello world"});
